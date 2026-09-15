4fd4347092d1218ba5b8c728bfa528957054552b
natnaelbefikadu55-hash <natnaelbefikadu55@gmail.com>
2026-09-16 00:57:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,21 +18,26 @@ let timer = 30;
 let jackpot = 1000;
 let drawnNumbers = [];
 let gameInterval = null;
+let timerInterval = null;
 let isGameRunning = false;
 
 function startNewGame() {
+  // ነባር ክፍለ-ጊዜዎችን (Intervals) ማጽዳት
+  if (gameInterval) clearInterval(gameInterval);
+  if (timerInterval) clearInterval(timerInterval);
+
   timer = 30;
   drawnNumbers = [];
   isGameRunning = false;
-  
+
   io.emit('game_reset', { message: "አዲስ ጨዋታ ሊጀምር ነው!" });
 
-  const countInterval = setInterval(() => {
+  timerInterval = setInterval(() => {
     timer--;
     io.emit('timer_update', { timer, jackpot });
 
     if (timer <= 0) {
-      clearInterval(countInterval);
+      clearInterval(timerInterval);
       startGameLoop();
     }
   }, 1000);
@@ -41,14 +46,15 @@ function startNewGame() {
 function startGameLoop() {
   isGameRunning = true;
   let allNumbers = Array.from({ length: 75 }, (_, i) => i + 1);
-  // Shuffle numbers
+  // ቁጥሮቹን በዘፈቀደ ማዋሃድ (Shuffle)
   allNumbers.sort(() => Math.random() - 0.5);
 
   gameInterval = setInterval(() => {
     if (allNumbers.length === 0 || !isGameRunning) {
       clearInterval(gameInterval);
+      isGameRunning = false;
       io.emit('game_over', { message: "ጨዋታው ተጠናቋል!" });
-      setTimeout(startNewGame, 5000); // 5 ሰከንድ ቆይቶ አዲስ ጨዋታ ይጀምራል
+      setTimeout(startNewGame, 5000); // ከ 5 ሰከንድ በኋላ አዲስ ዙር ይጀምራል
       return;
     }
 
@@ -60,17 +66,17 @@ function startGameLoop() {
       history: drawnNumbers
     });
 
-  }, 3000); // በየ 3 ሰከንዱ ቁጥር ያወጣል
+  }, 2500); // በየ 2.5 ሰከንዱ ቁጥር ያወጣል
 }
 
 io.on('connection', (socket) => {
-  socket.emit('init_state', { timer, jackpot, drawnNumbers });
+  socket.emit('init_state', { timer, jackpot, drawnNumbers, isGameRunning });
 
   // ተጫዋች BINGO ሲል
   socket.on('claim_bingo', (data) => {
     if (isGameRunning) {
       isGameRunning = false;
-      clearInterval(gameInterval);
+      if (gameInterval) clearInterval(gameInterval);
       
       io.emit('bingo_announced', {
         winner: socket.id,
@@ -78,7 +84,7 @@ io.on('connection', (socket) => {
         message: `🎉 BINGO! ካርቴላ #${data.cardId} አሸንፏል!`
       });
 
-      setTimeout(startNewGame, 7000); // ከ7 ሰከንድ በኋላ አዲስ ጨዋታ ያስጀምራል
+      setTimeout(startNewGame, 6000); // ከ 6 ሰከንድ በኋላ አዲስ ጨዋታ ያስጀምራል
     }
   });
 });
