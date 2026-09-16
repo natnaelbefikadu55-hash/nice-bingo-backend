962cf076e3b44b773035f8fff99cb0215acf520e
natnaelbefikadu55-hash <natnaelbefikadu55@gmail.com>
2026-09-17 00:37:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const io = new Server(server, {
   cors: { origin: "*", methods: ["GET", "POST"] }
 });
 
-// 1. መረጃዎች - Bot Token ከ Render Environment Variable ይወሰዳል
-const BOT_TOKEN = process.env.BOT_TOKEN;
+// 1. መረጃዎች - አዲሱን Token እዚህ ውስጥ ' ' መሃል ይተኩ
+const BOT_TOKEN = process.env.BOT_TOKEN || 'አዲሱን_TOKEN_እዚህ_ያስገቡ';
 const TELEBIRR_NUMBER = '0930488187';
 
 const bot = new Telegraf(BOT_TOKEN);
